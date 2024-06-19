e1d31b756e5adf4437175367cb11194803057d3c
ck-nm <168258507+ck-nm@users.noreply.github.com>
2024-06-19 09:29:26 +0000

    [finish]6/17bookshow,link

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,11 +26,12 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = @book.user
-    
+
   end
 
   def edit
     @book = Book.find(params[:id])
+    @user = current_user
   end
 
   def update
@@ -56,4 +57,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body )
   end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
+      redirect_to books_path
+    end
+  end
+
 end
