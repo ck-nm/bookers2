d9b52678705073eb575b4d99d7527844f2034af4
ck-nm <168258507+ck-nm@users.noreply.github.com>
2024-06-20 09:21:25 +0000

    [finish]6/20 prfimagelink

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     user_path(current_user)
   end
   def after_sign_out_path_for(resource)
-    about_path
+    root_path
   end
   
   protected
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
-  # 一覧ページ用のアクション
+
   def index
     @users = User.all
     @user = current_user
+    @book = Book.new
   end
 
   def edit
